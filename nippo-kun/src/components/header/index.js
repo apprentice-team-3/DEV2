502b6e8727b5897fd7f9442f8f4c1aaf273a3f46
gintoki <midoir041206@gmail.com>
2024-05-22 20:07:09 +0900

    にっぽーくんの表示と昨日立てた予定を表示する機能を追加

--- nippo-kun/src/components/header/index.js
+++ nippo-kun/src/components/header/index.js
@@ -1,19 +1,44 @@
-import React from 'react';
-import './index.css'; // スタイリングのためのCSSファイルをインポート
+import React, { useState } from 'react';
+import './index.css';
+import TodoList from './TodoList';
 
-const Header = () => {
-  return (
+
+
+function Header() {
+  const [yesterdayTasks, setYesterdayTasks] = useState(
+    localStorage.getItem("yesterday")
+      ? JSON.parse(localStorage.getItem("yesterday"))
+      : []
+  );
+
+  return(
     <header className="header">
       <h1 className="header-title">にっぽーくん</h1>
       <nav className="header-nav">
         <ul>
-          <li><a href="#home">昨日立てた予定</a></li>
-          <li><a href="#about">・Atcoder</a></li>
-          <li><a href="#contact">・提出クエスト</a></li>
+          <li>昨日立てた予定</li>
+          {yesterdayTasks.length > 0 ? (
+            <ul>
+              {yesterdayTasks.map((task, index) => (
+                <li key={index}>{task}</li>
+              ))}
+            </ul>
+          ) : (
+            <li>昨日のタスクがありません</li>
+          )}
         </ul>
       </nav>
     </header>
   );
 };
+
+// const Header = (props) => {
+//
+// const [todos, setTodos] = useState(["todo1", 'todo2']);
+//   return (
+//     <>
+//     <TodoList todos={todos} />
+//     </>
+//   );
 
 export default Header;
