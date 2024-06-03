c3de04fa1e7a132fd3b0d0a8578705ec1366531e
YNSTakeru <95395710+YNSTakeru@users.noreply.github.com>
2024-06-03 13:35:19 +0900

    developとmerge

--- nippo-kun/src/index.js
+++ nippo-kun/src/index.js
@@ -1,16 +1,20 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
+import { Provider } from "react-redux";
 import MetaData from "./components/MetaData";
 import Header from "./components/header";
 import PDCAList from "./components/pdcaList";
 import "./index.css";
 import reportWebVitals from "./reportWebVitals";
+import store from "./store";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
-    <Header />
-    <MetaData />
+    <Provider store={store}>
+      <Header />
+      <MetaData />
+    </Provider>
     <PDCAList />
   </React.StrictMode>
 );
