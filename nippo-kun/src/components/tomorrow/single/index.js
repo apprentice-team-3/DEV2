da2f76840402c7f5c7ceb37ce961d5ca33bd835b
gintoki <midoir041206@gmail.com>
2024-06-03 16:47:21 +0900

    cmd + enterを押すと+ボタンが押される処理と同様の処理ができるように変更

--- nippo-kun/src/components/tomorrow/single/index.js
+++ nippo-kun/src/components/tomorrow/single/index.js
@@ -1,7 +1,7 @@
 import { marked } from "marked";
-import React, { useState } from "react";
+import React, { useState, useRef, useEffect } from "react";
 import sanitizeHtml from "sanitize-html";
-import { useDispatch, useSelector } from 'react-redux';
+import { useDispatch, useSelector } from "react-redux";
 import { addTodo, removeTodo } from "../../../todoSlice";
 import "./index.css";
 
@@ -9,6 +9,7 @@ export default function MultiTextarea({ title, placeholder }) {
   const [content, setContent] = useState("");
   const todos = useSelector((state) => state.todo.tomorrow);
   const dispatch = useDispatch();
+  const textareaRef = useRef(null);
 
   const handleAddTextarea = () => {
     if(content.trim()) {
@@ -35,11 +36,34 @@ export default function MultiTextarea({ title, placeholder }) {
     return marked.parse(markedText);
   };
 
+    useEffect(() => {
+      const textarea  = textareaRef.current;
+      const handleKeyDown = (event) => {
+        if (event.key === 'Enter' && !event.isComposing) {
+          if (event.metaKey) {
+            event.preventDefault();
+            handleAddTextarea();
+          }
+        }
+      };
+
+      if (textarea) {
+        textarea.addEventListener('keydown', handleKeyDown);
+      }
+
+      return () => {
+        if (textarea) {
+          textarea.removeEventListener("keydown", handleKeyDown);
+        }
+      };
+    }, [handleAddTextarea]);
+
   return (
     <div>
       <h2 className="multi__title">{title}</h2>
       <div className="multi__textarea__wrapper">
         <textarea
+          ref={textareaRef}
           className="multi__textarea"
           placeholder={placeholder}
           value={content}
