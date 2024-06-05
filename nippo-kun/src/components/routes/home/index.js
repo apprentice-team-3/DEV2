a90a35217dddb5da469d4645dbd9ca6c73235b7c
YNSTakeru <95395710+YNSTakeru@users.noreply.github.com>
2024-06-05 21:59:13 +0900

    homeコンポーネントを更新

--- nippo-kun/src/components/routes/home/index.js
+++ nippo-kun/src/components/routes/home/index.js
@@ -1,6 +1,8 @@
 import MetaData from "../../MetaData";
+import AllCheckButton from "../../buttons/all-check-button";
 import Header from "../../header";
 import PDCA from "../../pdca";
+import Tomorrow from "../../tomorrow";
 
 const Home = () => {
   return (
@@ -8,6 +10,8 @@ const Home = () => {
       <Header />
       <MetaData />
       <PDCA />
+      <AllCheckButton />
+      <Tomorrow />
     </>
   );
 };
