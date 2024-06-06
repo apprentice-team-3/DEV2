31292de5a481e86c7328e45687a0d4e4cd61eb74
YNSTakeru <95395710+YNSTakeru@users.noreply.github.com>
2024-06-07 08:59:30 +0900

    テキストボックスをnotion化する処理の実装

---
+++ nippo-kun/src/components/pdca/single/Editor/index.js
@@ -0,0 +1,50 @@
+import { locales } from "@blocknote/core";
+import "@blocknote/core/fonts/inter.css";
+import { BlockNoteView } from "@blocknote/mantine";
+import "@blocknote/mantine/style.css";
+import { useCreateBlockNote } from "@blocknote/react";
+import { useDispatch } from "react-redux";
+import { write } from "../../../../redux/store/modules/pdcaList";
+
+export default function Editor({ order = "PDCA", setMarkdown }) {
+  const initialData = [{}];
+  const dispatch = useDispatch();
+
+  const editor = useCreateBlockNote({
+    initialContent: initialData,
+    dictionary: locales.ja,
+  });
+
+  const onChange = async () => {
+    const markdown = await editor.blocksToMarkdownLossy(editor.document);
+    setMarkdown(markdown);
+
+    switch (order) {
+      case "Plan":
+        dispatch(write({ plan: markdown }));
+        break;
+      case "Do":
+        dispatch(write({ do: markdown }));
+        break;
+      case "Check":
+        dispatch(write({ check: markdown }));
+        break;
+      case "Action":
+        dispatch(write({ action: markdown }));
+        break;
+      default:
+        break;
+    }
+  };
+
+  return (
+    <BlockNoteView
+      editor={editor}
+      onChange={onChange}
+      sideMenu={false}
+      slashMenu={true}
+      linkToolbar={false}
+      data-theming-css
+    />
+  );
+}
