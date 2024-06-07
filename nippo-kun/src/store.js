08c21530012233ec770c2b48c347605dffbe11c9
gintoki <midoir041206@gmail.com>
2024-06-07 23:00:55 +0900

    マージ後のエラーを修正

--- nippo-kun/src/store.js
+++ nippo-kun/src/store.js
@@ -1,16 +1,18 @@
 import { configureStore } from "@reduxjs/toolkit";
 import pdcaListReducer from "./redux/store/modules/pdcaList";
-import { rootReducer } from "./todoSlice";
+import { rootReducer, setDigressionMarkdown, setDigressionHTML, setHelpMarkdown, setHelpHTML } from './createMarkdownSlice';
 import todayReducer from './todaySlice';
+import todoReducer from './todoSlice';
 
 const store = configureStore({
   reducer: {
     todo: todoReducer,
     pdcaLister: pdcaListReducer,
     today: todayReducer,
-    reducer: rootReducer,
+    ...rootReducer,
   },
 });
 
 export default store;
 export * from './todoSlice';
+export { setDigressionMarkdown, setDigressionHTML, setHelpMarkdown, setHelpHTML };
--- nippo-kun/src/todoSlice.js
+++ nippo-kun/src/todoSlice.js
@@ -33,41 +33,6 @@ const todoSlice = createSlice({
     },
 });
 
-const createMarkdownSlice = (name) => {
-    return createSlice({
-        name,
-        initialState: { markdown: '', html: '' },
-        reducers: {
-            setMarkdown: (state, action) => {
-                state.markdown = action.payload;
-                const markedText = marked(action.payload, {
-                    gfm: true,
-                    breaks: true,
-                });
-                state.html = sanitizeHtml(markedText, {
-                    allowedTags: sanitizeHtml.defaults.allowedTags.concat(['img']),
-                    allowedAttributes: {
-                        a: ['href', 'name', 'target'],
-                        img: ['src', 'alt'],
-                    },
-                });
-            },
-            setHTML: (state, action) => {
-                state.html = action.payload;
-            }
-        },
-    });
-};
-
-const digressionSlice = createMarkdownSlice('digression');
-const helpSlice = createMarkdownSlice('help');
-
 export const { setTodo, addTodo, removeTodo, updateTodo } = todoSlice.actions;
-export const { setMarkdown: setDigressionMarkdown, setHTML: setDigressionHTML } = digressionSlice.actions;
-export const { setMarkdown: setHelpMarkdown, setHTML: setHelpHTML } = helpSlice.actions;
 
-export const rootReducer = {
-    todo: todoSlice.reducer,
-    digression: digressionSlice.reducer,
-    help: helpSlice.reducer,
-};
+export default todoSlice.reducer;
