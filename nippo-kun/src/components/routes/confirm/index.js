b19ecd6964c34763a9d29f0f52d6c0d310e307ed
YNSTakeru <95395710+YNSTakeru@users.noreply.github.com>
2024-06-09 07:27:07 +0900

    ai-review-buttonの実装

--- nippo-kun/src/components/routes/confirm/index.js
+++ nippo-kun/src/components/routes/confirm/index.js
@@ -12,41 +12,52 @@ const Confirm = () => {
   const isLoading = useSelector((state) => state.loader.isLoading);
   const [isStateLoading, setIsStateLoading] = useState(isLoading);
   const [review, setReview] = useState("");
+  const [firstTime, setFirstTime] = useState(true);
 
   const confirmReport = useSelector(
     (state) => state.confirmReporter.confirmReport
   );
   const dispatch = useDispatch();
 
-  useEffect(() => {
-    setTimeout(() => {
-      if (isLoading) {
-        try {
-          fetch("https://express-hello-world-a3nc.onrender.com/all-review/", {
-            method: "POST",
-            headers: {
-              "Content-Type": "application/json",
-            },
-            body: JSON.stringify({
-              value: confirmReport,
-              order: "Action",
-            }),
-          }).then((response) => {
-            if (response.ok) {
-              response.json().then((data) => {
-                const { generatedText } = data;
-                dispatch(reviewWrite(generatedText));
-                setReview(marked.parse(generatedText));
-                setIsStateLoading(false);
-              });
-            }
-          });
-        } catch (err) {
-          console.error("AIのレビューに失敗しました", err);
+  async function fetchReview(confirmReport) {
+    console.log(confirmReport);
+    try {
+      setIsStateLoading(true);
+      const response = await fetch(
+        "https://express-hello-world-a3nc.onrender.com/all-review/",
+        {
+          method: "POST",
+          headers: {
+            "Content-Type": "application/json",
+          },
+          body: JSON.stringify({
+            value: confirmReport,
+            order: "Action",
+          }),
         }
+      );
+      if (response.ok) {
+        const data = await response.json();
+        const { generatedText } = data;
+        dispatch(reviewWrite(generatedText));
+        setReview(marked.parse(generatedText));
+        setIsStateLoading(false);
       }
-    }, 1000);
-  }, [isLoading]);
+    } catch (err) {
+      console.error("AIのレビューに失敗しました", err);
+    }
+  }
+
+  useEffect(() => {
+    if (confirmReport && firstTime) {
+      fetchReview(confirmReport);
+      setFirstTime(false);
+    }
+  }, [confirmReport]);
+
+  const onClick = () => {
+    fetchReview(confirmReport);
+  };
 
   return (
     <>
@@ -60,6 +71,9 @@ const Confirm = () => {
                 __html: review,
               }}
             ></div>
+            <button className="review-button" onClick={onClick}>
+              再レビュー
+            </button>
           </div>
         )}
         <Textarea />
