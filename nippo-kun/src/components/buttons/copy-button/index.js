6c4876a0311ee05da8056ff8aa7f72ff134f82ad
YNSTakeru <95395710+YNSTakeru@users.noreply.github.com>
2024-06-09 03:50:48 +0900

    クリップボードにコピーされると昨日立てた予定が更新されるように修正

--- nippo-kun/src/components/buttons/copy-button/index.js
+++ nippo-kun/src/components/buttons/copy-button/index.js
@@ -5,12 +5,16 @@ export default function Copy() {
   const confirmReport = useSelector(
     (state) => state.confirmReporter.confirmReport
   );
+
+  const tomorrowTodo = useSelector((state) => state.todo.tomorrow);
+
   const onClick = async (e) => {
     e.preventDefault();
 
     try {
       await navigator.clipboard.writeText(confirmReport);
-      console.log("テキストがクリップボードにコピーされました");
+      if (tomorrowTodo.length === 0) return;
+      localStorage.setItem("yesterday", JSON.stringify(tomorrowTodo));
     } catch (err) {
       console.error("テキストのコピーに失敗しました", err);
     }
