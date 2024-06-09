51c89793b1702fb787573c77a26d1becfeab36e6
gintoki <midoir041206@gmail.com>
2024-06-09 10:20:13 +0900

    明日やる事リストのローカルストレージへの保存機能を削除しました

--- nippo-kun/src/todoSlice.js
+++ nippo-kun/src/todoSlice.js
@@ -3,13 +3,10 @@ import { createSlice } from "@reduxjs/toolkit";
 const yesterdayTasks = localStorage.getItem("yesterday")
   ? JSON.parse(localStorage.getItem("yesterday"))
   : [];
-const storedTomorrow = localStorage.getItem("tomorrow")
-  ? JSON.parse(localStorage.getItem("tomorrow"))
-  : [];
 
 const initialState = {
   todo: yesterdayTasks[0],
-  tomorrow: storedTomorrow,
+  tomorrow: [],
 };
 
 const todoSlice = createSlice({
@@ -21,18 +18,15 @@ const todoSlice = createSlice({
     },
     addTodo: (state, action) => {
       state.tomorrow.push(action.payload);
-      localStorage.setItem("tomorrow", JSON.stringify(state.tomorrow));
     },
     removeTodo: (state, action) => {
       state.tomorrow = state.tomorrow.filter(
         (_, index) => index !== action.payload
       );
-      localStorage.setItem("tomorrow", JSON.stringify(state.tomorrow));
     },
     updateTodo: (state, action) => {
       const { index, content } = action.payload;
       state.tomorrow[index] = content;
-      localStorage.setItem("tomorrow", JSON.stringify(state.tomorrow));
     },
   },
 });
